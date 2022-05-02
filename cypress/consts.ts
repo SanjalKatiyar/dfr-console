65fb669d6c567038981ccd541bd12447b9440c8b
Pranshu Srivastava <rexagod@gmail.com>
2022-05-02 23:17:33 +0530

    Integrate Cypress
    Adds cypress support for running e2e tests in an OCP cluster.
    
    Signed-off-by: Pranshu Srivastava <rexagod@gmail.com>

---
+++ cypress/consts.ts
@@ -0,0 +1,7 @@
+export const KUBEADMIN_USERNAME = 'kubeadmin';
+export const KUBEADMIN_IDP = 'kube:admin';
+export const MANAGEDMCG_NAME = 'managedmcg';
+export const NAMESPACE = '';
+
+export const SECOND = 1000;
+export const MINUTE = 60 * SECOND;
